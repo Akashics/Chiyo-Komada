af0f632ca05676745c82e11bfae4732a41ab509e
Kashall <Kashalls@users.noreply.github.com>
2017-08-07 22:32:50 -0700

    Fixes

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-const keys = require('./keys.json');
-const Raven = require('raven');
+const keys = require("./keys.json");
+const Raven = require("raven");
 const Komada = require("komada");
 
-Raven.config('https://8d8ad27176784fba9f8ad42e58f22e1b:db9053e0c2944602bdca9b00abc8bd9f@sentry.io/196284', {
-  release: '0.1.35',
-  logger: 'default',
-  environment: 'production'
+Raven.config("https://8d8ad27176784fba9f8ad42e58f22e1b:db9053e0c2944602bdca9b00abc8bd9f@sentry.io/196284", {
+  release: "0.1.35",
+  logger: "default",
+  environment: "production"
 }).install();
 
 const client = new Komada.Client({
