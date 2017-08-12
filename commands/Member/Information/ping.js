9b7211f0c32fa3f912ce81a215c05846b22e0515
Kashall <Kashalls@users.noreply.github.com>
2017-08-12 11:11:53 -0700

    Actually fixed ping command

--- commands/Member/Information/ping.js
+++ commands/Member/Information/ping.js
@@ -1,32 +1,32 @@
 exports.run = async (client, msg) => {
-  const now = require("performance-now");
-  const req = require("snekfetch");
-  const x = now();
+	const now = require('performance-now');
+	const req = require('snekfetch');
+	const x = now();
 
-  const ram = async () => {
-    const first = await now();
-    await req.get(`http://rra.ram.moe/i/r`)
-    return (now() - first)
-  }
+	const ram = async () => {
+		const first = await now();
+		await req.get('http://rra.ram.moe/i/r');
+		return (now() - first);
+	};
 
-  const ani = async () => {
-    const first = await now();
-    await req.get("https://anilist.co/")
-    return (now() - first)
-  }
+	const ani = async () => {
+		const first = await now();
+		await req.get('https://anilist.co/');
+		return (now() - first);
+	};
 
-  const neko = async () => {
-    const first = await now();
-    await req.get("http://catgirls.brussell98.tk/api/random")
-    return (now() - first)
-  };
+	const neko = async () => {
+		const first = await now();
+		await req.get('http://catgirls.brussell98.tk/api/random');
+		return (now() - first);
+	};
 
-  const check = async () => {
-    const a = await ram();
-    const b = await ani();
-    const c = await neko();
+	const check = async () => {
+		const a = await ram();
+		const b = await ani();
+		const c = await neko();
 
-    return msg.channel.send(`
+		return msg.channel.send(`
 = Calculated Response Times =
 
 Heart Beat       :: ${Math.round(client.ping)}ms
@@ -36,25 +36,25 @@ Anilist API      :: ${Math.round(b)}ms
 Neko API         :: ${Math.round(c)}ms
 Chiyo Dashboard  :: N/A
 
-Total Calc Time  :: ${Math.round(now() - x)}ms`, { code: "asciidoc" });
+Total Calc Time  :: ${Math.round(now() - x)}ms`, { code: 'asciidoc' });
 
-  };
-  check()
+	};
+	check();
 };
 
 exports.conf = {
-  enabled: true,
-  runIn: ["text"],
-  aliases: [],
-  permLevel: 0,
-  botPerms: [],
-  requiredFuncs: [],
-  requiredSettings: [],
+	enabled: true,
+	runIn: ['text'],
+	aliases: [],
+	permLevel: 0,
+	botPerms: [],
+	requiredFuncs: [],
+	requiredSettings: [],
 };
 
 exports.help = {
-  name: "ping",
-  description: "Ping/Pong. I wonder what this does? /sarcasm",
-  usage: "",
-  usageDelim: "",
+	name: 'ping',
+	description: 'Ping/Pong. I wonder what this does? /sarcasm',
+	usage: '',
+	usageDelim: '',
 };
