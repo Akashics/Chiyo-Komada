e1a4f7eaa9af17b3833deb74f27f5de7bf093f58
Kashall <Kashalls@users.noreply.github.com>
2017-08-12 11:11:36 -0700

    Updated url to support

--- commands/Member/Information/support.js
+++ commands/Member/Information/support.js
@@ -1,21 +1,21 @@
-exports.run = async (client, msg, [...args]) => {
-    return msg.send(`Hi @${msg.author.toString()}, if you need support with anything Discord related or with Chiyo, you can ask in Chiyo's Academy. https://discord.gg/V7whPwb`);
-}
+exports.run = async (client, msg) => {
+	return msg.send(`Hi @${msg.author.toString()}, if you need support with anything Discord related or with Chiyo, you can ask in Chiyo's Academy. https://discord.gg/V7whPwb`);
+};
 
 exports.conf = {
-    enabled: true,
-    runIn: ["text"],
-    aliases: [],
-    permLevel: 0,
-    botPerms: [],
-    requiredFuncs: [],
-    cooldown: 3,
+	enabled: true,
+	runIn: ['text'],
+	aliases: [],
+	permLevel: 0,
+	botPerms: [],
+	requiredFuncs: [],
+	cooldown: 3,
 };
 
 exports.help = {
-    name: "support",
-    description: "So you need some help.",
-    usage: "",
-    usageDelim: "",
-    extendedHelp: "",
+	name: 'support',
+	description: 'So you need some help.',
+	usage: '',
+	usageDelim: '',
+	extendedHelp: '',
 }; 
