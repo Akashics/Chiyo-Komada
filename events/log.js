666471ef4bd49948f58266a3a1f72d8cd6380827
Kashall <Kashalls@users.noreply.github.com>
2017-08-12 12:43:28 -0700

    eslint

--- events/log.js
+++ events/log.js
@@ -1,46 +1,46 @@
-const moment = require("moment");
-const chalk = require("chalk");
+const moment = require('moment');
+const chalk = require('chalk');
 const Raven = require('raven');
 
 const clk = new chalk.constructor({ enabled: true });
 
 /* eslint-disable no-use-before-define */
-exports.run = (client, data, type = "log") => {
-  type = type.toLowerCase();
+exports.run = (client, data, type = 'log') => {
+	type = type.toLowerCase();
 
-  data = data.stack || data.message || data;
-  if (typeof data === "object" && typeof data !== "string" && !Array.isArray(data)) data = require("util").inspect(data, { depth: 0, colors: true });
-  if (Array.isArray(data)) data = data.join["\n"];
+	data = data.stack || data.message || data;
+	if (typeof data === 'object' && typeof data !== 'string' && !Array.isArray(data)) data = require('util').inspect(data, { depth: 0, colors: true });
+	if (Array.isArray(data)) data = data.join['\n'];
 
-  let timestamp = "";
-  if (!client.config.disableLogTimestamps) {
-    timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]`;
-    if (!client.config.disableLogColor) {
-      switch (type) {
-        case "debug":
-          timestamp = clk.bgMagenta(timestamp);
-          break;
-        case "warn":
-          timestamp = clk.black.bgYellow(timestamp);
-          Raven.captureException(data, {
-            level: 'warning'
-          });
-          break;
-        case "error":
-          timestamp = clk.bgRed(timestamp);
-          Raven.captureException(data, {
-            level: 'error'
-          });
-          break;
-        case "log":
-          timestamp = clk.bgBlue(timestamp);
-          break;
-        // no default
-      }
-    }
-    timestamp += " ";
-  }
+	let timestamp = '';
+	if (!client.config.disableLogTimestamps) {
+		timestamp = `[${moment().format('YYYY-MM-DD HH:mm:ss')}]`;
+		if (!client.config.disableLogColor) {
+			switch (type) {
+			case 'debug':
+				timestamp = clk.bgMagenta(timestamp);
+				break;
+			case 'warn':
+				timestamp = clk.black.bgYellow(timestamp);
+				Raven.captureException(data, {
+					level: 'warning'
+				});
+				break;
+			case 'error':
+				timestamp = clk.bgRed(timestamp);
+				Raven.captureException(data, {
+					level: 'error'
+				});
+				break;
+			case 'log':
+				timestamp = clk.bgBlue(timestamp);
+				break;
+								// no default
+			}
+		}
+		timestamp += ' ';
+	}
 
-  if (type === "debug") type = "log";
-  console[type](data.split("\n").map(str => timestamp + str).join("\n"));
+	if (type === 'debug') type = 'log';
+	console[type](data.split('\n').map(str => timestamp + str).join('\n'));
 };
