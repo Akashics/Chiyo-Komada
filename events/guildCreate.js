fe6420b11f11334e37387ed37107f628a4386c1c
Kashall <Kashalls@users.noreply.github.com>
2017-08-13 11:37:06 -0700

    Fixed moment

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -15,10 +15,13 @@ exports.run = (client, guild) => {
 			'(╯°□°）╯︵ ┻━┻, Member for 10+ Minutes',
 			'┻━┻︵ヽ(`Д´)ﾉ︵┻━┻, Needs Verified Phone'
 		];
+		const moment = require('moment');
+		require('moment-duration-format');
+		const day = moment(guild.createdAt).format('MM-DD-YYYY HH:mm a');
 
 		const embed = new client.methods.Embed()
 			.setTitle('Chiyo was added to a guild!')
-			.setAuthor(`${guild.name} -`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
+			.setAuthor(`${guild.name} (${guild.memberCount})`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
 			.setColor(0x0AC120)
 			.setFooter('', 'http://i.imgur.com/w1vhFSR.png')
 			.setThumbnail(guild.iconURL() || 'http://i.imgur.com/escBXVB.png')
@@ -26,10 +29,9 @@ exports.run = (client, guild) => {
 			.setURL('https://chiyo.ml')
 			.addField('**Guild Name**', `${guild.name}`, true)
 			.addField('**Guild ID**', `${guild.id}`, true)
-			.addField('**Guild Region**', `${guild.region}`, true)
+			.addField('**Guild Region**', `${guild.region.toUpperCase()}`, true)
 			.addField('**Guild Owner**', `${guild.owner.user.tag} (${guild.owner.id})`, true)
-			.addField('**Guild Count**', `${guild.memberCount}`, true)
-			.addField('**Guild Created**', `${guild.createdAt}`, true)
+			.addField('**Guild Created**', `${day}`, true)
 			.addField('**Guild Settings**', `Ver: ${guildVerification[guild.verificationLevel] || 'Unknown'}; Filter: ${guildFilter[guild.explicitContentFilter] || 'Unknown'}`, true);
 		return client.channels.get('341768632545705986').send({ embed: embed });
 	}
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,9 +3,13 @@ exports.run = (client, guild) => {
 	client.user.setGame(`${keys.botInfo.prefix}help || ${client.guilds.size} guilds`);
 	if (guild.available) {
 
+		const moment = require('moment');
+		require('moment-duration-format');
+		const day = moment(Date.now()).format('MM-DD-YYYY HH:mm a');
+
 		const embed = new client.methods.Embed()
 			.setTitle('Chiyo was removed from a guild.')
-			.setAuthor(`${guild.name} -`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
+			.setAuthor(`${guild.name} (${guild.memberCount})`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
 			.setColor(0xCC0000)
 			.setFooter('', 'http://i.imgur.com/w1vhFSR.png')
 			.setThumbnail(guild.iconURL() || 'http://i.imgur.com/escBXVB.png')
@@ -13,10 +17,10 @@ exports.run = (client, guild) => {
 			.setURL('https://chiyo.ml')
 			.addField('**Guild Name**', `${guild.name}`, true)
 			.addField('**Guild ID**', `${guild.id}`, true)
-			.addField('**Guild Region**', `${guild.region}`, true)
+			.addField('**Guild Region**', `${guild.region.toUpperCase()}`, true)
 			.addField('**Guild Owner**', `${guild.owner.user.tag} (${guild.owner.id})`, true)
 			.addField('**Guild Count**', `${guild.memberCount}`, true)
-			.addField('**Guild Created**', `${guild.createdAt}`, true);
+			.addField('**Guild Deleted**', `${day}`, true);
 		return client.channels.get('341768632545705986').send({ embed: embed });
 	}
 };
