d5ff2708be7954f58bed323c79cfb16be1971e94
Kashall <Kashalls@users.noreply.github.com>
2017-08-25 22:10:00 -0700

    Getting ready to add cleverbot

---
+++ commands/Member/Quotes/ai.js
@@ -0,0 +1,23 @@
+exports.run = async (client, msg) => {
+
+
+
+};
+
+exports.conf = {
+	enabled: false,
+	runIn: ['text'],
+	aliases: [],
+	permLevel: 0,
+	botPerms: [],
+	requiredFuncs: [],
+	cooldown: 3,
+};
+
+exports.help = {
+	name: 'ai',
+	description: 'Talk with Cleverbot!',
+	usage: '',
+	usageDelim: '',
+	extendedHelp: '',
+}; 
