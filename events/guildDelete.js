7d3349e563b61e77db5a5c4cbcde3bd8d615ca20
Kashall <Kashalls@users.noreply.github.com>
2017-08-27 17:20:14 -0700

    Events are now under 3 complexity [Codacy]

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,25 +1,13 @@
 exports.run = (client, guild) => {
 	if (guild.available) {
-		//Lets update the listings!
 		client.funcs.discordListUpdate.update(client);
-		const moment = require('moment');
-		require('moment-duration-format');
-		const day = moment(Date.now()).format('MM-DD-YYYY HH:mm a');
-
 		const embed = new client.methods.Embed()
-			.setTitle('Chiyo was removed from a guild.')
-			.setAuthor(`${guild.name} (${guild.memberCount})`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
+			.setAuthor(`${guild.name} (${guild.id})`, (guild.iconURL() || 'http://i.imgur.com/escBXVB.png'))
+			.addField('**Guild Owner**', `${guild.owner.user.tag}\n_${guild.owner.id}_`, true)
+			.addField('**Guild Count**', `${guild.memberCount} users`, true)
 			.setColor(0xCC0000)
-			.setFooter('', 'http://i.imgur.com/w1vhFSR.png')
-			.setThumbnail(guild.iconURL() || 'http://i.imgur.com/escBXVB.png')
 			.setTimestamp()
-			.setURL('https://chiyo.ml')
-			.addField('**Guild Name**', `${guild.name}`, true)
-			.addField('**Guild ID**', `${guild.id}`, true)
-			.addField('**Guild Region**', `${guild.region.toUpperCase()}`, true)
-			.addField('**Guild Owner**', `${guild.owner.user.tag} (${guild.owner.id})`, true)
-			.addField('**Guild Count**', `${guild.memberCount}`, true)
-			.addField('**Guild Deleted**', `${day}`, true);
+			.setThumbnail(guild.iconURL() || 'http://i.imgur.com/escBXVB.png');
 		return client.channels.get('341768632545705986').send({ embed: embed });
 	}
 };
